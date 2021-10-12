2175653c7af773d321335ab80fa52d827687e486
rizkyzhang <41568558+rizkyzhang@users.noreply.github.com>
2021-10-12 16:42:41 +0700

    feat: add gallery to explore projects

--- src/App.js
+++ src/App.js
@@ -1,94 +1,19 @@
 import React from "react";
-import {
-  Box,
-  Container,
-  Text,
-  HStack,
-  VStack,
-  Image,
-  Heading,
-  Button,
-  Circle,
-  Flex,
-  Spacer,
-} from "@chakra-ui/react";
 
-import { Icon } from "@chakra-ui/icons";
-import { FaMusic } from "react-icons/fa";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-import Hero from "./images/illustration-hero.svg";
-import Desktop from "./images/pattern-background-desktop.svg";
+import OrderSummary from "./pages/OrderSummary";
+import Home from "./pages/Home";
 
 function App() {
   return (
-    <>
-      <Container
-        display="flex"
-        alignItems="center"
-        justifyContent="center"
-        maxWidth="full"
-        height="100vh"
-        bgImage={Desktop}
-        bgColor="hsl(225, 100%, 94%)"
-        bgPosition="top"
-        bgRepeat="no-repeat"
-        fontFamily="Red Hat Display, sans-serif"
-      >
-        <Box
-          maxWidth="xs"
-          bgColor="white"
-          borderRadius="lg"
-          boxShadow="2xl"
-          fontSize="sm"
-        >
-          <Image
-            src={Hero}
-            mb={5}
-            borderTopLeftRadius="lg"
-            borderTopRightRadius="lg"
-          />
-          <Flex alignItems="center" direction="column" p={5} textAlign="center">
-            <Heading mb={4} fontSize="xl">
-              Order Summary
-            </Heading>
-            <Text mb={8} color="gray.500">
-              You can now listen to millions of songs, audiobooks, and podcast
-              on any device anywhere you like!
-            </Text>
-
-            <HStack mb={8} spacing={5}>
-              <Circle size={10} bg="hsl(225, 100%, 94%)" color="gray.500">
-                <Icon as={FaMusic}></Icon>
-              </Circle>
-              <VStack spacing={0}>
-                <Text fontWeight="bold">Annual Plan</Text>
-                <Text color="gray.500">$59.99/year</Text>
-              </VStack>
-              <Spacer></Spacer>
-              variant="link"
-              <Button variant="link" color="blue" fontSize="xs">
-                Change
-              </Button>
-            </HStack>
-
-            <Button
-              mb={4}
-              bg=" hsl(245, 75%, 52%)"
-              boxShadow="2xl"
-              color="white"
-              fontSize="inherit"
-              _hover={{ boxShadow: "dark-lg" }}
-              isFullWidth
-            >
-              Proceed to Payment
-            </Button>
-            <Button variant="ghost" fontSize="xs">
-              Cancel Order
-            </Button>
-          </Flex>
-        </Box>
-      </Container>
-    </>
+    <Router>
+      <Switch>
+        <Route exact path="/" component={Home} />
+        <Route exact path="/fem/order-summary" component={OrderSummary} />
+        <Route path="*" component={Home} />
+      </Switch>
+    </Router>
   );
 }
 
---
+++ src/pages/Home/index.js
@@ -0,0 +1,88 @@
+import React from "react";
+
+import {
+  Container,
+  SimpleGrid,
+  Flex,
+  Box,
+  Image,
+  Button,
+  Heading,
+  Text,
+  Link,
+  chakra,
+} from "@chakra-ui/react";
+
+import { Link as ReactLink } from "react-router-dom";
+
+import Projects from "./projects.json";
+
+function Home() {
+  return (
+    <Container maxWidth="full" p={7} fontFamily="Nunito, sans-serif">
+      <Heading as="h1" fontSize={35} color="blue.400" textAlign="center" mb={7}>
+        Chakra UI Projects
+      </Heading>
+      <SimpleGrid minChildWidth={300} spacing={10} justifyItems="center">
+        {Projects.map((project) => (
+          <Box
+            maxWidth="xs"
+            bgColor="white"
+            borderRadius="lg"
+            boxShadow="dark-lg"
+            fontSize="sm"
+          >
+            <Image
+              src={project.image}
+              borderTopLeftRadius="lg"
+              borderTopRightRadius="lg"
+            />
+            <Flex
+              alignItems="center"
+              direction="column"
+              p={5}
+              textAlign="center"
+            >
+              <Heading as="h2" fontSize={25} mb={5}>
+                {project.name}
+              </Heading>
+              <Text mb={2}>
+                <chakra.span
+                  bgGradient="linear-gradient(45deg, #ffb74d, #fc5857)"
+                  bgClip="text"
+                  fontFamily="Dancing Script"
+                  fontSize="lg"
+                  fontWeight="bold"
+                >
+                  designed
+                </chakra.span>{" "}
+                by{" "}
+                <Link
+                  href={project.designLink}
+                  color="blue.400"
+                  fontWeight="bold"
+                >
+                  {project.designer}
+                </Link>
+              </Text>
+              <Text mb={5}>Completed at {project.completedAt}</Text>
+              <Button
+                as={ReactLink}
+                to={project.projectLink}
+                bgColor="blue.400"
+                color="white"
+                font="inherit"
+                isFullWidth
+                _hover={{ boxShadow: "dark-lg" }}
+              >
+                View
+              </Button>
+            </Flex>
+          </Box>
+        ))}
+      </SimpleGrid>
+    </Container>
+  );
+}
+
+export default Home;
